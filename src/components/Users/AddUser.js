e5eab2d779620d6e7a2169a71d86aa2c63d20946
johncovenant <ufotjohnamos@gmail.com>
2023-04-25 16:38:44 +0100

    AddUser CSS

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -6,13 +6,15 @@ const AddUser = props => {
     };
 
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" />
-            <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number" />
-            <button type="submit">Add User</button>
-        </form>
+        <Card>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text" />
+                <label htmlFor="age">Age (Years)</label>
+                <input id="age" type="number" />
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
     );
 
 };
