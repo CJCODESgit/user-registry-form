819d279a774baf32ca1b9537dfb4dd141f36e393
johncovenant <ufotjohnamos@gmail.com>
2023-04-25 18:53:30 +0100

    card bug fixed

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Card from "../UI/Card";
+import classes from './AddUser.module.css';
+
+
 
 const AddUser = props => {
     const addUserHandler = (event) => {
@@ -7,7 +10,7 @@ const AddUser = props => {
     };
 
     return (
-        <Card>
+        <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
                 <input id="username" type="text" />
