3a9fd8c018c7c3edaaca355540acb27c8c881074
johncovenant <ufotjohnamos@gmail.com>
2023-04-28 15:07:59 +0100

    added validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -12,6 +12,12 @@ const AddUser = props => {
 
     const addUserHandler = (event) => {
         event.preventDefault();
+        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            return;
+        }
+        if (+enteredAge < 1) {
+            return;
+        }
         console.log(enteredUsername, enteredAge);
         setEnteredUsername('')
         setEnteredAge('');
