84d0cc5c844145ff3244f6d71f0fa65aab5d693b
johncovenant <ufotjohnamos@gmail.com>
2023-04-28 16:35:24 +0100

    managing input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+
   return (
     <div>
-      <AddUser />
-      <UsersList users={[]} />
+      <AddUser onAddUser />
+      <UsersList users={usersList} />
     </div>
   );
 }
