973f606b98b4231e4761d166479f9320c7b542c8
johncovenant <ufotjohnamos@gmail.com>
2023-04-28 15:44:11 +0100

    UsersList.module.css

--- src/components/Users/UsersList.jsx
+++ src/components/Users/UsersList.jsx
@@ -1,1 +1,14 @@
 import React from "react";
+
+const UsersList = props => {
+    return (
+        <ul>
+          {props.users.map(user =>
+             <li>
+                {user.name} ({user.age}years old)</li>
+                )}  
+        </ul>
+    );
+};
+
+export default UsersList;
