356c857a060470ce0d7ddec0074b25cb6ef33271
johncovenant <ufotjohnamos@gmail.com>
2023-04-28 15:59:50 +0100

    Userlist component completed and added to App.js

--- src/components/Users/UsersList.jsx
+++ src/components/Users/UsersList.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import classes from './UsersList.module.css'
 import Card from "../UI/Card";
 
 const UsersList = props => {
     return (
-        <Card>
+        <Card className={classes.users}>
         <ul>
           {props.users.map(user =>
              <li>
