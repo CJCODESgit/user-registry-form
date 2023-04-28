fca73122b1d61977cdce1008f652b11d70c9b1e6
johncovenant <ufotjohnamos@gmail.com>
2023-04-28 15:46:35 +0100

    using CARD component wrapper

--- src/components/Users/UsersList.jsx
+++ src/components/Users/UsersList.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
+import Card from "../UI/Card";
+
 const UsersList = props => {
     return (
+        <Card>
         <ul>
           {props.users.map(user =>
              <li>
                 {user.name} ({user.age}years old)</li>
                 )}  
         </ul>
+        </Card>
     );
 };
 
