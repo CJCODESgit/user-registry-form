1cd4bfa1bfea663a3534fbbdf9a2d3284eaef1fe
johncovenant <ufotjohnamos@gmail.com>
2023-04-28 16:45:27 +0100

    added user

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import UsersList from './components/Users/UsersList';
 function App() {
   const [usersList, setUsersList] = useState([]);
 
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, { name: uName, age: uAge }]
+    });
+  }
 
   return (
     <div>
-      <AddUser onAddUser />
+      <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
     </div>
   );
