7e9c9ec2c4b1ec90a57e45c80ca503b6425b8f26
johncovenant <ufotjohnamos@gmail.com>
2023-04-30 23:41:07 +0100

    error handler

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -42,9 +42,12 @@ const AddUser = props => {
         setEnteredAge(event.target.value);
     }
 
+    const errorHandler = () => {
+        setError(null);
+    };
     return (
         <div>
-       { error && <ErrorModal title= {error.title} message={error.message} /> }
+       { error && <ErrorModal title= {error.title} message={error.message} onConfirm={errorHandler}/> }
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
