017892795c05aa5c221e567bb41605c17a118a21
johncovenant <ufotjohnamos@gmail.com>
2023-04-30 18:03:07 +0100

    Modal frame

--- src/components/UI/ErrorModal.jsx
+++ src/components/UI/ErrorModal.jsx
@@ -1,8 +1,22 @@
 import React from "react";
-
+import classes from './ErrorModal.module.css'
+import Card from "./Card";
+import Button from './Button'
 
 const ErrorModal = props => {
+return (
+    <Card>
+        <header>
+            <h2></h2>
+        </header>
+        <div>
+            <p></p>
+        </div>
+        <footer>
 
+        </footer>
+    </Card>
+);
 };
 
 export default ErrorModal;
