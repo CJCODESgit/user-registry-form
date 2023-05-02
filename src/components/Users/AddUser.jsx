1ec7178d92130ae1d2231960c3713676b929fbb5
johncovenant <ufotjohnamos@gmail.com>
2023-05-02 15:11:27 +0100

    wrapper rendered

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
+import Wrapper from '../Helpers/Wrapper';
 import classes from './AddUser.module.css';
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
@@ -46,7 +47,7 @@ const AddUser = props => {
         setError(null);
     };
     return (
-        <div>
+        <Wrapper>
        { error && <ErrorModal title= {error.title} message={error.message} onConfirm={errorHandler}/> }
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
@@ -57,7 +58,7 @@ const AddUser = props => {
                 <Button type="submit">Add User</Button>
             </form>
         </Card >
-        </div>
+        </Wrapper>
     );
 
 };
